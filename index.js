902d7cca152817c4beaf733965d4027fa5caff83
Martin Perussini <mperussini@frsf.utn.edu.ar>
2022-11-11 01:03:02 -0300

    fifth commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 1234
+const port = process.env.PORT || 1234
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/prueba', (req, res) => {
+  res.send('Pruebaaaaaaa')
 })
 
 app.listen(port, () => {
