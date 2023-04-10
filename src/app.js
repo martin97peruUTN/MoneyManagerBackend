25ff3279c675de0e243b5b2449eb6d1a7cabc5ec
Martin Perussini <mperussini@frsf.utn.edu.ar>
2023-04-10 16:57:21 -0300

    DB connection

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import authenticateToken from './middleware/JWTAuthentication.js';
 import { login } from './routes/login.js';
 import { tests } from './routes/tests.js';
 
+import { connectionDB } from './db.js';
 //const connectDB = require('./db.js')
 //connectDB()
 
@@ -21,6 +22,22 @@ app.use('/api', authenticateToken)
 //Routes
 app.use(login)
 app.use(tests)
+
+// Este ahora no anda porque le puse el /promise al mysql2
+// app.get('/dbtest', (req, res) => {
+// 	connectionDB.query('SELECT 1 + 1 AS Suma', (err, results, fields) => {
+// 		if (err) {
+// 			console.log(err)
+// 			res.status(500).send('Error en la base de datos')
+// 		}
+// 		res.status(200).send(results)
+// 	})
+// })
+
+app.get('/dbtest2', async (req, res) => {
+	const [result] = await connectionDB.query('SELECT 1 + 1 AS result')
+	res.status(200).json(result[0])
+})
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port} 😎 🤙`)
--- src/db.js
+++ src/db.js
@@ -1,13 +1,17 @@
-require('dotenv').config()
-import { createConnection } from 'mysql2'
+import dotenv from 'dotenv';
+dotenv.config();
+//con el /promise le digo que use promesas
+import { createConnection, createPool } from 'mysql2/promise'
 
-function connectDB(){
-    const connectionDB = createConnection(process.env.DATABASE_URL)
-    console.log('Connected to PlanetScale!')
+// function connectDB(){
+//     const connectionDB = createConnection(process.env.DATABASE_URL)
+//     console.log('Connected to PlanetScale!')
     
-    const suma = connectionDB.query('SELECT 1 + 1 AS Suma')
-    console.log(suma);//no anda, qsy
-}
+//     const suma = connectionDB.query('SELECT 1 + 1 AS Suma')
+//     console.log(suma);//no anda, qsy
+// }
 
-//connection.end()
-export default connectDB
+// //connection.end()
+// export default connectDB
+
+export const connectionDB = createPool(process.env.DATABASE_URL)
